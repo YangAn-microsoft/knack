7943f4efef19cbc9634747525a0070a32db897c6
Yang An <yangan@microsoft.com>
2026-06-09 13:58:47 +1000

    {Packaging} Add Python 3.15 support

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3.15',
         'License :: OSI Approved :: MIT License',
     ],
     packages=['knack', 'knack.testsdk'],
